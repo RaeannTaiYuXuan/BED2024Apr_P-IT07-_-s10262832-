ce8c98ec891ae1cdf26e24c321ae71994525702a
RaeannTaiYuXuan <S10262832@connect.np.edu.sg>
2024-05-09 11:51:50 +0800

    Completed

--- Week_04/books-api-mvc-db/app.js
+++ Week_04/books-api-mvc-db/app.js
@@ -2,13 +2,20 @@ const express = require("express");
 const booksController = require("./controllers/booksController");
 const sql = require("mssql");
 const dbConfig = require("./dbConfig");
+const bodyParser = require("body-parser");
+const validateBook = require("./middlewares/validateBook") // Import body-parser
 
 const app = express();
-const port = process.env.PORT || 3000; // Use environment variable or default port
+const port = 3000;
 
-// Routes for GET requests (replace with appropriate routes for update and delete later)
+// Include body-parser middleware to handle JSON data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
 app.get("/books", booksController.getAllBooks);
 app.get("/books/:id", booksController.getBookById);
+app.post("/books", validateBook, booksController.createBook); // POST for creating books (can handle JSON data)
+app.put("/books/:id", booksController.updateBook); // PUT for updating books
+app.delete("/books/:id", booksController.deleteBook); // DELETE for deleting books
 
 app.listen(port, async () => {
   try {
